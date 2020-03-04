6711ad3c9f8498082b7cb553ab442a883ff260a1
eadehemingway <eade.hemingway@gmail.com>
2020-03-04 12:40:34 +0000

    create gradient visual legend

--- src/components/GradientVisual.tsx
+++ src/components/GradientVisual.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as d3 from "d3";
 
 export default function Gradient({ data }) {
+  useEffect(() => {
+    const svg = d3
+      .select("#Gradient-svg")
+      .attr("width", 700)
+      .attr("height", 400);
+
+    const linearGradient = svg
+      .append("defs")
+      .append("linearGradient")
+      .attr("id", "linear-gradient");
+
+    linearGradient
+      .selectAll("stop")
+      .data(data.items)
+      .enter()
+      .append("stop")
+      .attr("offset", (d, i) => {
+        const offset = i * (100 / data.items.length);
+        return `${offset}%`;
+      })
+      .attr("stop-color", d => d.color);
+
+    svg
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", 50)
+      .attr("width", 700)
+      .attr("height", 10)
+      .style("fill", "url(#linear-gradient)");
+
+    svg
+      .selectAll("text")
+      .data(data.items)
+      .enter()
+      .append("text")
+      .text(d => d.name)
+      .attr("x", (d, i) => {
+        const x = i * (100 / data.items.length + 1);
+        return `${x}%`;
+      })
+      .attr("y", 80);
+  }, [data.items]);
+
   return (
     <>
-      <p>Gradient</p>
+      <svg id="Gradient-svg"></svg>
     </>
   );
 }
